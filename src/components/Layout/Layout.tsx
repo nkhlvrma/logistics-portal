472bc818eb82007b5df464477065624d69d4af00
Nikhil Kumar <nkhlvrma@gmail.com>
2025-10-26 23:03:08 +0530

    fix css

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -32,8 +32,10 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
   useEffect(() => {
     if (darkMode) {
       document.body.classList.add("dark-mode");
+      document.body.classList.remove("light-mode-override");
     } else {
       document.body.classList.remove("dark-mode");
+      document.body.classList.add("light-mode-override");
     }
     localStorage.setItem("darkMode", darkMode ? "true" : "false");
   }, [darkMode]);
